2752ea76cddb0cce122f5c74286c3abc3950b7e2
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-11 14:19:07 +0300

    Add Fetch Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
+import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
-      <UseStateArray />
+      <FetchData />
     </div>
   );
 }
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,39 @@
+import { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch(url);
+        const profile = await response.json();
+        setProfile(profile);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProfile();
+  }, [profile])
+  return (
+    <section>
+      <h3>github users</h3>
+      <ul className='users'>
+        {profile.map((profile) => {
+          const { id, login, avatar_url, html_url } = profile;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
